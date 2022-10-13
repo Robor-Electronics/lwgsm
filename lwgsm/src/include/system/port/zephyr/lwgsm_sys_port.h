f2a321b3cccfb02c07b2d2b15a8e4be6e6213462
Mathijs de Groot <m.degroot@robor.nl>
2022-10-13 11:05:10 +0200

    Implemented zephyr sys port header

--- lwgsm/src/include/system/port/zephyr/lwgsm_sys_port.h
+++ lwgsm/src/include/system/port/zephyr/lwgsm_sys_port.h
@@ -7,8 +7,96 @@
 #ifndef LWLIBPROJECT_LWGSM_SYS_PORT_H
 #define LWLIBPROJECT_LWGSM_SYS_PORT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #if LWGSM_CFG_OS && !__DOXYGEN__
 
+#include <zephyr/kernel.h>
+
+/**
+ * \brief           System mutex type
+ *
+ * It is used by middleware as base type of mutex.
+ */
+typedef struct k_mutex lwgsm_sys_mutex_t;
+
+/**
+ * \brief           System semaphore type
+ *
+ * It is used by middleware as base type of mutex.
+ */
+typedef struct k_sem lwgsm_sys_sem_t;
+
+/**
+ * \brief           System message queue type
+ *
+ * It is used by middleware as base type of mutex.
+ */
+typedef struct k_mbox lwgsm_sys_mbox_t;
+
+/**
+ * \brief           System thread ID type
+ */
+typedef struct k_thread lwgsm_sys_thread_t;
+
+/**
+ * \brief           System thread priority type
+ *
+ * It is used as priority type for system function,
+ * to start new threads by middleware.
+ */
+typedef int lwgsm_sys_thread_prio_t;
+
+/**
+ * \brief           Mutex invalid value
+ *
+ * Value assigned to \ref lwgsm_sys_mutex_t type when it is not valid.
+ */
+#define LWGSM_SYS_MUTEX_NULL  ((lwgsm_sys_mutex_t)0)
+
+/**
+ * \brief           Semaphore invalid value
+ *
+ * Value assigned to \ref lwgsm_sys_sem_t type when it is not valid.
+ */
+#define LWGSM_SYS_SEM_NULL    ((lwgsm_sys_sem_t)0)
+
+/**
+ * \brief           Message box invalid value
+ *
+ * Value assigned to \ref lwgsm_sys_mbox_t type when it is not valid.
+ */
+#define LWGSM_SYS_MBOX_NULL   ((lwgsm_sys_mbox_t)0)
+
+/**
+ * \brief           OS timeout value
+ *
+ * Value returned by operating system functions (mutex wait, sem wait, mbox wait)
+ * when it returns timeout and does not give valid value to application
+ */
+#define LWGSM_SYS_TIMEOUT (-EAGAIN)
+
+/**
+ * \brief           Default thread priority value used by middleware to start built-in threads
+ *
+ * Threads can well operate with normal (default) priority and do not require
+ * any special feature in terms of priority for prioer operation.
+ */
+#define LWGSM_SYS_THREAD_PRIO 0
+
+/**
+ * \brief           Stack size in units of bytes for system threads
+ *
+ * It is used as default stack size for all built-in threads.
+ */
+#define LWGSM_SYS_THREAD_SS   (1024)
+
 #endif /* LWGSM_CFG_OS && !__DOXYGEN__ */
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif //LWLIBPROJECT_LWGSM_SYS_PORT_H
