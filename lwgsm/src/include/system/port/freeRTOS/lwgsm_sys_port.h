666b305af3612f40ba47da83baf5eaec6810b97e
Rick Overhorst <r.overhorst@robor.nl>
2025-04-28 11:49:36 +0200

    Implemented feedback

--- lwgsm/src/include/system/port/freeRTOS/lwgsm_sys_port.h
+++ lwgsm/src/include/system/port/freeRTOS/lwgsm_sys_port.h
@@ -133,6 +133,7 @@ typedef UBaseType_t lwgsm_sys_thread_prio_t;
  * It is used as default stack size for all built-in threads.
  */
 #define LWGSM_SYS_THREAD_SS   (1440)
+#define LWGSM_PROCESS_THREAD_SS  (200)
 
 #endif /* LWGSM_CFG_OS && !__DOXYGEN__ */
 
